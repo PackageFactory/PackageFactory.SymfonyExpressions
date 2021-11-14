81ab8012314e25ca2bc2bc21d5ba904b9e2263ee
Martin Ficzel <ficzel@sitegeist.de>
2021-11-14 12:43:51 +0100

    TASK: Support default helpers and fluent interfaces like flowQuery

--- Classes/Evaluator/SymfonyExpressionEvaluator.php
+++ Classes/Evaluator/SymfonyExpressionEvaluator.php
@@ -6,7 +6,8 @@ namespace PackageFactory\SymfonyExpressions\Evaluator;
 use Neos\Flow\Annotations as Flow;
 use Neos\Cache\Frontend\VariableFrontend;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
-use PackageFactory\SymfonyExpressions\ExpressionFunctionProvider\HelperExpressionFunctionProvider;
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
 
 /**
  * @Flow\Scope("singleton")
@@ -15,9 +16,10 @@ class SymfonyExpressionEvaluator
 {
 
     /**
-     * @var ExpressionLanguage
+     * @var VariableFrontend
+     * @Flow\Inject
      */
-    protected $expressionLanguage;
+    protected $parsedExpressionCache;
 
     /**
      * @Flow\InjectConfiguration(path="helpers")
@@ -26,36 +28,157 @@ class SymfonyExpressionEvaluator
     protected $helperConfiguration;
 
     /**
-     * @var VariableFrontend
-     * @Flow\Inject
+     * @Flow\InjectConfiguration(path="providers")
+     * @var array
      */
-    protected $parsedExpressionCache;
+    protected $providerConfiguration;
+
+    /**
+     * @Flow\InjectConfiguration(path="functions")
+     * @var array
+     */
+    protected $functionConfiguration;
+
+    /**
+     * @var ExpressionLanguage
+     */
+    protected $expressionLanguage;
+
+    /**
+     * @var array<string,mixed>
+     */
+    protected $defaultContextVariables = [];
 
     public function initializeObject()
     {
         $this->expressionLanguage = new ExpressionLanguage();
-        foreach ($this->helperConfiguration as $name => $helperConfig) {
-            $this->expressionLanguage->registerProvider(
-                new HelperExpressionFunctionProvider($name, $helperConfig)
-            );
+        $functions = $this->createHelperFunctions($this->functionConfiguration);
+        foreach($functions as $function) {
+            $this->expressionLanguage->addFunction($function);
         }
+        $providers = $this->createFunctionProviders($this->providerConfiguration);
+        foreach($providers as $provider) {
+            $this->expressionLanguage->registerProvider($provider);
+        }
+        $this->defaultContextVariables = $this->createDefaultContextVariables($this->helperConfiguration);
     }
 
     /**
+     * Evaluate the given expression
+     *
      * @param string $expression
      * @param array $context
      * @return mixed
      */
-    public function evaluate(string $expression, array $context)
+    public function evaluate(string $expression, array $context = [])
     {
-        $cacheIdentifier = md5($expression . ':' . implode(',',array_keys($context)));
+        $mergedContext = array_merge($context, $this->defaultContextVariables);
+        $cacheIdentifier = md5($expression . ':' . implode(',',array_keys($mergedContext)));
         if ($parsedExpression = $this->parsedExpressionCache->get($cacheIdentifier)) {
-            return $this->expressionLanguage->evaluate($parsedExpression, $context);
+            return $this->expressionLanguage->evaluate($parsedExpression, $mergedContext);
         }
-        // this is needed as otherwise the parser will cast all nodes to string which crashes for complex objects
-        $contextWithNullValues = array_map(function($item) { return null;}, $context);
-        $parsedExpression = $this->expressionLanguage->parse($expression, $contextWithNullValues);
+        $parsedExpression = $this->expressionLanguage->parse($expression, array_keys($mergedContext));
         $this->parsedExpressionCache->set($cacheIdentifier, $parsedExpression);
-        return $this->expressionLanguage->evaluate($parsedExpression, $context);
+        return $this->expressionLanguage->evaluate($parsedExpression, $mergedContext);
+    }
+
+    /**
+     * Create the ExpressionFunctions for the configured functions
+     *
+     * @see: https://symfony.com/doc/current/components/expression_language/extending.html
+     * @param array $configuration
+     * @return ExpressionFunction[]
+     */
+    public function createHelperFunctions(array $configuration): array
+    {
+        $expressionFunctions = [];
+        foreach ($configuration as $expressionFunctionName => $functionPath) {
+            if (strpos($functionPath, '::') !== false) {
+                list($className, $methodName) = explode('::', $functionPath, 2);
+                $compiler = function (...$args) use ($methodName, $className) {
+                    return sprintf('\%s::%s(%s)', $className, $methodName, implode(', ', $args));
+                };
+                $evaluator = function ($p, ...$args) use ($methodName, $className) {
+                    return $className::$methodName(...$args);
+                };
+                $expressionFunctions[] = new ExpressionFunction(
+                    $expressionFunctionName,
+                    $compiler,
+                    $evaluator
+                );
+            } else {
+                $expressionFunctions = ExpressionFunction::fromPhp($functionPath,$expressionFunctionName);
+            }
+        }
+        return $expressionFunctions;
+    }
+
+    /**
+     * Instantiate the configured ExpressionFunctionProviders
+     *
+     * @see: https://symfony.com/doc/current/components/expression_language/extending.html
+     * @param array $configuration
+     * @return ExpressionFunctionProviderInterface[]
+     */
+    public function createFunctionProviders(array $configuration): array
+    {
+        $expressionFunctionProviders = [];
+        foreach ($configuration as $objectType) {
+            if (is_null($objectType)) {
+                continue;
+            }
+            $provider = new $objectType();
+            if (!$provider instanceof ExpressionFunctionProviderInterface) {
+                throw new \InvalidArgumentException(sprintf("Expression function provider %s does not implement %s", $objectType, ExpressionFunctionProviderInterface::class));
+            }
+            $expressionFunctionProviders[] = new $objectType();
+        }
+        return $expressionFunctionProviders;
+    }
+
+    /**
+     * Get default context variables from configuration that should be set in the context of all expressions by default.
+     *
+     * @param array $configuration An one dimensional associative array of context variable paths mapping to object names
+     * @return array Array with default context variable objects.
+     */
+    public function createDefaultContextVariables(array $configuration): array
+    {
+        $defaultContextVariables = [];
+        foreach ($configuration as $variableName => $objectType) {
+            $variablePathNames = explode('.', $variableName);
+            $pathDepth = count($variablePathNames);
+            $currentDepth = 1;
+            $currentHelper = null;
+            foreach ($variablePathNames as $pathName) {
+                // directly assign key to context array
+                if (($currentDepth === 1) && ($pathDepth === 1)) {
+                    $defaultContextVariables[$pathName] = new $objectType();
+                    continue;
+                }
+
+                // build tree of stdClass objects
+                if ($currentDepth == 1) {
+                    if (isset($defaultContextVariables[$pathName])) {
+                        $currentHelper = $defaultContextVariables[$pathName];
+                    } else {
+                        $currentHelper = new \stdClass();
+                        $defaultContextVariables[$pathName] = $currentHelper;
+                    }
+                } else {
+                    if ($currentDepth < $pathDepth) {
+                        if ($currentHelper->$pathName) {
+                            $currentHelper = $currentHelper->$pathName;
+                        } else {
+                            $currentHelper->$pathName = new \stdClass();
+                        }
+                    } else {
+                        $currentHelper->$pathName = new $objectType();
+                    }
+                }
+                $currentDepth ++;
+            }
+        }
+        return $defaultContextVariables;
     }
 }
--- README.md
+++ README.md
@@ -3,60 +3,64 @@
 
 !!! This is an experimental prototype to validate the feasibility of using symfony-expressions in fusion as an alternative to eel. It should not be used in projects !!!
 
+The symfony expressions language is documented by the symfomy project https://symfony.com/doc/current/components/expression_language.html 
+and offers a very well defined syntax with operations that go beyond what eel offers https://symfony.com/doc/current/components/expression_language/syntax.html
+
 ## Usage 
 
-The symfony expressions language is documented by the symfomy project https://symfony.com/doc/current/components/expression_language.html
-There are many differences and similarities to the eel language of which the most notable ones ones are:
+In Neos symfony-expressions are written like eel expressions with a prefix of `${sx:...}`.
+The most important deviation from eel is that Strings are concatenated via `~` like so `${"Hello" + "world"}` becomes `${sx: "Hello" ~ "world"}`.
+The `sx:` prefix works in afx aswell ``afx`<tag atttribute={sx:...}>{sx:...}</tag>` ``.
+Even the fluent syntax of FlowQuery is supported `${sx:q(node).closest('[instanceof Neos.Neos:Document]').property("title")}`.
 
-- Strings are concatenated via `~`
-- Helper are functions not Objects 
+Note: The `sx:` prefix was the easiest way to implement a working prototype for Fusion and Afx. 
+If this ever becomes a language feature this should be discussed in detail and probably another solution will be found. 
 
-In Neos symfony-expressions are written like eel expressions with a prefix of `${sx:...}`. 
-This prefix works in afx aswell ``afx`<tag atttribute={sx:...}>{sx:...}</tag>` ``. 
+## Extensibility: Helpers, Function and Providers
 
-Note: The `sx:` prefix was the easiest way to implement a working prototype for Fusion and Afx. If this ever becomes a 
-language feature this should be discussed in detail and probably another solution will be found. 
-
-## Helpers
-
-Similar to eel the symfony expression language can be extended but other than eel extensions for symfony expression are functions. 
-We added a function provider that allows to register helper classes that provide static helper functions. All functions of the 
-helper classes are registered to the symfony expression language by combining the given prefix with the function name using CamelCase.
-
-The example demonstrates the use of the `toUpperCase` function in the `String` helper.
-```
-renderer = afx`
-    <div>
-        {sx:StringToUpperCase("hello world")}
-    </div>
-`
-```
-
-The package contains some ports of eel helpers to test the extensibility: 
+The package allows to register functions, function providers and helpers which allows to mimic the behavior of eel in most parts.
 ```
 PackageFactory:
   SymfonyExpressions:
-    helpers:
-      
-      #
-      # Those helper are port of eel helpers with static methods
-      #
-      String: 'PackageFactory\SymfonyExpressions\Helper\StringHelper'
-      Array: 'PackageFactory\SymfonyExpressions\Helper\ArrayHelper'
-      Date: 'PackageFactory\SymfonyExpressions\Helper\DateHelper'
-      
-      #
-      # Not working yet, how do we define fluent interfaces ;-/ 
-      # 
-      FlowQuery: 'PackageFactory\SymfonyExpressions\Helper\FlowQueryHelper'
-```
 
-Note: We are still looking for a way of defining fluent interfaces like flowQuery for symfony expressions.
+    #
+    # Class names of helper objects that should always be present in the expression context.
+    #
+    helpers:
+      String: 'Neos\Eel\Helper\StringHelper'
+      Array: 'Neos\Eel\Helper\ArrayHelper'
+      Date: 'Neos\Eel\Helper\DateHelper'
+      Configuration: 'Neos\Eel\Helper\ConfigurationHelper'
+      Math: 'Neos\Eel\Helper\MathHelper'
+      Json: 'Neos\Eel\Helper\JsonHelper'
+      Security: 'Neos\Eel\Helper\SecurityHelper'
+      Translation: 'Neos\Flow\I18n\EelHelper\TranslationHelper'
+      StaticResource: 'Neos\Flow\ResourceManagement\EelHelper\StaticResourceHelper'
+      Type: 'Neos\Eel\Helper\TypeHelper'
+      I18n: 'Neos\Flow\I18n\EelHelper\TranslationHelper'
+      File: 'Neos\Eel\Helper\FileHelper'
+      BaseUri: 'Neos\Fusion\Eel\BaseUriHelper'
+
+    #
+    # Class names of function providers implementing the ExpressionFunctionProviderInterface
+    # @see: https://symfony.com/doc/current/components/expression_language/extending.html
+    #
+    providers: []
+
+    #
+    # Functions that are either static functions with namespaces or static methods of classes 
+    # - static class method  `name: 'Name\Space::method'`
+    # - static function `name: 'Name\Space\method'``
+    # 
+    functions:
+      q: 'Neos\Eel\FlowQuery\FlowQuery::q'
+
+```
 
 ## Installation
 
 For now the package is available via github only. https://github.com/PackageFactory/PackageFactory.SymfonyExpressions
-If you want to try participate in the experiment add this to your `composer.json`
+If you want to participate in the experiment add this to your `composer.json`
 
 ```
 {
@@ -98,8 +102,9 @@ The expression cache can be flushed via `./flow flow:cache:flushone PackageFacto
  
 The most important limitations we are currently aware of are:
  
-- The implementation via a Fusion object may or may not prove problematic especially performance wise. 
-- The missing support (or finding the correct syntax) for fluent interfaces limits the actual use.
+- The implementation via a Fusion object may or may not prove problematic especially performance wise.
+- Multiline expressions will not work (yet)
+- The ContextAware interface and other security limitations of eel are not on place (yet). 
 - The parsed Expressions are cached but for now the cache is never flushed.
 
 ## Contribution
